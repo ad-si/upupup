d7414864a7f2e4259af03dda3634402bfdf78314
Adrian Sieber <mail@adriansieber.com>
2015-03-28 15:26:46 +0100

    Upgrade to lastes version of formidable & http-auth

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ var formidable = require('formidable'),
 	express = require('express'),
 	app = express(),
 
-	basic = auth({
-		authRealm: "Private area.",
-		authList: ['guest:iamavisitor7']
-	})
+	basic = auth.basic(
+		{
+			realm: "Adrian's Server"
+		},
+		function (username, password, callback) {
+			callback(
+				username === process.env.upupup_user &&
+				password === process.env.upupup_password
+			)
+		}
+	)
 
+
+app.use(auth.connect(basic))
 
 app.get('/', function (request, response) {
-	basic.apply(request, response, function () {
-		response.render('index.jade')
-	})
+	response.render('index.jade')
 })
 
 
@@ -23,96 +30,93 @@ app.get('/upload', function (request, response) {
 })
 app.post('/upload', function (request, response) {
 
-	basic.apply(request, response, function () {
-
-		var form = new formidable.IncomingForm(),
-			files = [],
-			fields = [],
-			directoryIsCreated = false,
-			directoryPath
+	var form = new formidable.IncomingForm(),
+		files = [],
+		fields = [],
+		directoryIsCreated = false,
+		directoryPath
 
 
-		function makeDir(callback) {
+	function makeDir(callback) {
 
-			var directoryName = new Date()
-				.toJSON()
-				.replace(/:/g, '-')
-				.slice(0, 19)
+		var directoryName = new Date()
+			.toJSON()
+			.replace(/:/g, '-')
+			.slice(0, 19)
 
-			directoryPath = path.join(__dirname, 'files', directoryName)
+		directoryPath = path.join(__dirname, 'files', directoryName)
 
-			fs.mkdir(
-				directoryPath,
-				function (error) {
-					if (error &&
-						error.message.search('file already exists') !== -1) {
+		fs.mkdir(
+			directoryPath,
+			function (error) {
+				if (error &&
+					error.message.search('file already exists') !== -1) {
 
-						console.error(error.message)
-						makeDir(callback)
-					}
-					else
-						callback()
-				})
-		}
-
-		function onField(field, value) {
-
-			console.log(field, value)
-
-			fields.push({
-				name: field,
-				value: value
-			})
-		}
-
-		function onFile(field, file) {
-
-			if (file.size > 0) {
-
-				files.push({
-					name: file.name,
-					type: file.type,
-					size: file.size
-				})
-
-				if (!directoryIsCreated)
-					makeDir(function () {
-						moveFile(file)
-					})
-				else
-					moveFile(file)
-			}
-		}
-
-		function moveFile(file) {
-
-			var newFilePath = path.join(
-				directoryPath,
-				file.name
-					.toLowerCase()
-					.replace(/ /g, '-')
-			)
-
-			fs.rename(
-				file.path,
-				newFilePath,
-				function (error) {
-					if (error) throw error
+					console.error(error.message)
+					makeDir(callback)
 				}
-			)
+				else
+					callback()
+			})
+	}
+
+	function onField(field, value) {
+
+		console.log(field, value)
+
+		fields.push({
+			name: field,
+			value: value
+		})
+	}
+
+	function onFile(field, file) {
+
+		if (file.size > 0) {
+
+			files.push({
+				name: file.name,
+				type: file.type,
+				size: file.size
+			})
+
+			if (!directoryIsCreated)
+				makeDir(function () {
+					moveFile(file)
+				})
+			else
+				moveFile(file)
 		}
+	}
 
-		function onEnd() {
-			response.render('upload.jade', {files: files})
-		}
+	function moveFile(file) {
 
-		form
-			.on('field', onField)
-			.on('file', onFile)
-			.on('end', onEnd)
+		var newFilePath = path.join(
+			directoryPath,
+			file.name
+				.toLowerCase()
+				.replace(/ /g, '-')
+		)
 
-		form.parse(request)
-	})
+		fs.rename(
+			file.path,
+			newFilePath,
+			function (error) {
+				if (error) throw error
+			}
+		)
+	}
+
+	function onEnd() {
+		response.render('upload.jade', {files: files})
+	}
+
+	form
+		.on('field', onField)
+		.on('file', onFile)
+		.on('end', onEnd)
+
+	form.parse(request)
 })
 
 module.exports = app
