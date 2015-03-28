035bc6040c3238d1f4dc4e5397be234f857c582d
Adrian Sieber <mail@adriansieber.com>
2015-03-28 21:33:38 +0100

    Add index view of uploaded files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var path = require('path'),
 	express = require('express'),
+	serveIndex = require('serve-index'),
 
 	upupup = require('./app.js'),
 
@@ -8,6 +9,9 @@ var path = require('path'),
 
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/files', express.static(path.join(__dirname, 'files')))
+app.use('/files', serveIndex('files', {icons: true}))
 
 app.use(upupup)
 
